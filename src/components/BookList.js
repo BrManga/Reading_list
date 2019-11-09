60642f54952470849bbd1082ff93c5c8ff234240
Bora Manga <bora.mng525@gmail.com>
2019-11-09 23:17:57 +0100

    localstorage property added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,16 +4,14 @@ import BookDetails from "../components/BookDetails";
 const BookList = () => {
   const { books } = useContext(BookContext);
   return books.length ? (
-    <ul>
-      <li className="book-list">
-        {books.map(book => {
-          return (
-            <BookDetails book={book} key={book.id}>
-              {" "}
-            </BookDetails>
-          );
-        })}
-      </li>
+    <ul className="book-list">
+      {books.map(book => {
+        return (
+          <BookDetails book={book} key={book.id}>
+            {" "}
+          </BookDetails>
+        );
+      })}
     </ul>
   ) : (
     <div className="empty">No books to read</div>
